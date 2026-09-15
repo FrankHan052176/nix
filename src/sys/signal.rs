417f57fcf5b446ed8838b07233a9e81a9382268b
韩嘉乐 <2382008060@qq.com>
2026-09-16 03:45:39 +0800

    ohos: cover SigevThreadId in SigEvent::new for target_env = ohos

--- src/sys/signal.rs
+++ src/sys/signal.rs
@@ -1069,6 +1069,8 @@ mod sigevent {
                 SigevNotify::SigevThreadId{..} => libc::SIGEV_THREAD_ID,
                 #[cfg(all(target_os = "linux", target_env = "uclibc"))]
                 SigevNotify::SigevThreadId{..} => libc::SIGEV_THREAD_ID,
+                #[cfg(all(target_os = "linux", target_env = "ohos"))]
+                SigevNotify::SigevThreadId{..} => 4, // No SIGEV_THREAD_ID defined
                 #[cfg(any(all(target_os = "linux", target_env = "musl"), target_arch = "mips"))]
                 SigevNotify::SigevThreadId{..} => 4  // No SIGEV_THREAD_ID defined
             };
